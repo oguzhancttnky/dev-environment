9c56d0bd311384971c86ee9592ec2bb3cce5a4cf
Oguzhan <oguzhancttnky@gmail.com>
2025-01-14 14:58:42 +0300

    updated home with global variable

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap
 set fish_greeting ""
 
 # pnpm
-set -gx PNPM_HOME "/home/oguzhan/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
