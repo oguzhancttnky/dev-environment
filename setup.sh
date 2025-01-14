c9e54c83c041310968ee989b305f6ffb75aa0625
Oguzhan <oguzhancttnky@gmail.com>
2025-01-14 16:07:14 +0300

    added docker setup

--- setup.sh
+++ setup.sh
@@ -13,13 +13,17 @@ sudo apt-get update
 sudo apt-get install -y fish
 chsh -s /usr/bin/fish
 
-echo "---Installing necessary tools---"
+echo "---Installing necessary tools (net-tools, ca-certificates, curl, wget, snapd, build-essential, python3, python3-pip, docker, openjdk, nodejs, pnpm, go)---"
 
-sudo apt-get install -y net-tools curl wget snapd
-
-sudo apt-get install -y gcc
+sudo apt-get install -y net-tools ca-certificates curl wget snapd build-essential
 
 sudo apt-get install -y python3 python3-pip
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+bash -c 'echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable"' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 JAVA_VERSION=$(ask_version "Java" "21")
 echo "---Installing Java $JAVA_VERSION---"
