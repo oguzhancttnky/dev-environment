c9ee53df38378504047590cf16a755f2c88f1b30
Oguzhan <oguzhancttnky@gmail.com>
2025-01-15 09:39:39 +0300

    search history updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,2 @@
 set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin $HOME/go/bin /usr/local/go/bin
 set fish_greeting ""
-
-bind \cR history-search-backward
-bind \cS history-search-forward
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,35 @@
-function _fzf_search_history
-    history | fzf --height=40% --reverse | read -l command
-    if test -n "$command"
-        commandline -r $command
+function _fzf_search_history --description "Search command history."
+
+    if test -z "$fish_private_mode"
+        builtin history merge
     end
+
+    if not set -q fzf_history_time_format
+        set -g fzf_history_time_format "%m-%d %H:%M:%S"
+    end
+
+    set -l time_prefix_regex '^.*? │ '
+
+    set -l commands_selected (
+        builtin history --null --show-time="$fzf_history_time_format │ " |
+        fzf --read0 \
+            --print0 \
+            --multi \
+            --prompt="History> " \
+            --query=(commandline) \
+            --preview="string replace --regex '$time_prefix_regex' '' -- {} | fish_indent --ansi" \
+            --preview-window="bottom:3:wrap" \
+            --bind="ctrl-r:execute(echo {q})+abort" \
+            $fzf_history_opts |
+        string split0 |
+        string replace --regex $time_prefix_regex ''
+    )
+
+    if test $status -eq 0
+        commandline --replace -- $commands_selected
+    end
+
+    commandline --function repaint
 end
 
 bind \cr '_fzf_search_history'
