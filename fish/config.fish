1c0adc22bc285880bd9817b444f1670fd4561297
Oguzhan <oguzhancttnky@gmail.com>
2025-01-26 21:35:29 +0300

    added path of cargo

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
-set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin $HOME/go/bin /usr/local/go/bin
+set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin $HOME/go/bin /usr/local/go/bin $HOME/.cargo/bin
 set fish_greeting ""
