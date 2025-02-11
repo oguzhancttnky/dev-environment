b399977d05a0e5a36125905f809f4160115b65f2
Oguzhan <oguzhancttnky@gmail.com>
2025-02-11 19:10:51 +0300

    updated paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
-set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin /opt/gradle/gradle-8.12.1/bin
+set -gxp PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin /opt/gradle/gradle-8.12.1/bin $HOME/go/bin /usr/local/go/bin $HOME/.cargo/bin $HOME/.local/bin
 set fish_greeting ""
