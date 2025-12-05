0fb7bab3e4c8f2aff13ab5b4dec89dc5c9aa704c
oguzhancttnky <oguzhancttnky@gmail.com>
2025-12-05 19:01:49 +0300

    chore: update tools

--- setup.sh
+++ setup.sh
@@ -63,6 +63,10 @@ chown -R $USER:$USER ~/.config/fish
 fish -c 'curl -sL https://git.io/fisher | source; fisher install jorgebucaran/fisher'
 fish -c "fisher install (cat ./fish/fish_plugins)"
 fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No"
+cp fish/fish-ai.ini ~/.config/
+
+echo "---Installing Ollama---"
+curl -fsSL https://ollama.com/install.sh | sh
 
 # Snap Installs
 echo "---Installing Tools from Snap---"
